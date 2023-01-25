8eab1c60b85f79ca0239415045c4b57408079476
LyzhenkovAlexandr <74377794+LyzhenkovAlexandr@users.noreply.github.com>
2023-01-25 22:34:13 +0300

    first commit

---
+++ src/Sum.java
@@ -0,0 +1,32 @@
+public class Sum {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (String arg : args) {
+            char[] char_string = arg.toCharArray();
+            StringBuilder num = new StringBuilder();
+            for (int i = 0; i < char_string.length; i++) {
+                if (!Character.isWhitespace(char_string[i])) {
+                    if (i < char_string.length - 1) {
+                        num.append(char_string[i]);
+                    } else if (i == char_string.length - 1) {
+                        num.append(char_string[i]);
+                        sum += Integer.parseInt(num.toString());
+                        num = new StringBuilder();
+                    }
+                } else if (!num.toString().equals("")) {
+                    sum += Integer.parseInt(num.toString());
+                    num = new StringBuilder();
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
+
+
+
+
+
+
+
